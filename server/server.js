097bf225451e482491d46ef5bccfa01471bdcf96
eavalo14 <avaloserick97@gmail.com>
2023-06-04 14:43:55 -0600

    removed cors legacy code

--- server/server.js
+++ server/server.js
@@ -2,23 +2,13 @@
 import path from "path"
 import express from 'express'
 import routes from './routes/index.js'
-import cors from 'cors'
 
 // instantiate express and port 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
 
 // instatiate express middleware routes
 app.use(routes);
-
-// TODO: remove in production
-// cors middleware 
-app.use(cors(corsOptions));
 
 // start server
 app.listen(PORT, () => console.log('Now listening'));
