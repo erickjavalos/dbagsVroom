57b551a285fb33ac0022d9e175602058ce089401
eavalo14 <avaloserick97@gmail.com>
2023-06-04 14:56:51 -0600

    integrated wallet connectivity with discord authentication

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+import { fromText, Lucid,Blockfrost } from "lucid-cardano";
+import Header from '../../components/Header';
+// TODO: may have to hide this api key later on for security reasons
+const lucid = await Lucid.new(
+    new Blockfrost("https://cardano-preprod.blockfrost.io/api/v0", "preprod537IKjHMy3Pots7uk6KKitjlUyIKGdw0"),
+    "Preprod",
+  );
+
+function Home() {
+    const [walletConnected, setWalletConnected] = useState(null);
+
+    // Function to update walletConnected in the App component
+    const updateWalletConnected = async (wallet) => {
+      setWalletConnected(wallet);
+      lucid.selectWallet(wallet);
+      console.log(await lucid.wallet.address())
+    };
+
+
+    const processMintRequest = async () => {
+      console.log('minting')
+      // get hashed metadata
+
+      const address = await lucid.wallet.address();
+      console.log(address)
+      // const payload = fromText("Hello from Lucid!");
+
+      // const tx = await lucid.newTx()
+      // console.log(tx)
+    }
+
+    return (
+
+        <>
+            <Header updateWalletConnected={updateWalletConnected}/>
+
+            {walletConnected === null ? 
+              <h1>Wallet not connected</h1> 
+              :
+              <section className="bg-gray-200 p-4 flex justify-center items-center">
+                <div className="container mx-auto">
+                  <button 
+                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={processMintRequest}
+                  >
+                    Mint
+                  </button>
+                </div>
+              </section>
+            }
+
+        </>
+    )
+}
+
+export default Home;
