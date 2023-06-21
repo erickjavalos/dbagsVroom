171c5bfb7fc3eb0f17fd1ff7be5ab649915fb523
eavalo14 <avaloserick97@gmail.com>
2023-06-20 19:22:15 -1000

    adding front end wallet support

---
+++ client/config.js
@@ -0,0 +1,5 @@
+const blockfrostApiKey = {
+    0: "preprodEfV6mA9d1Mkavc5XRYmFGsPdqAfj1HGx", // testnet
+}
+
+export default blockfrostApiKey;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,58 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "build"),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-        template: path.join(__dirname, "public", "index.html"),
-        }),
-    ],
-    devServer: {
-        static: {
-        directory: path.join(__dirname, "build"),
-        },
-        port: 3000,
-    },
-    module: {
-        // exclude node_modules
-        rules: [
-          {
-            test: /\.(js|jsx)$/,         // <-- added `|jsx` here
-            exclude: /node_modules/,
-            use: ["babel-loader"],
-          },
-          {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader', 'postcss-loader'],
-          },
-          {
-            test: /\.(jpe?g|png|gif|woff|woff2|otf|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-            use: [
-                {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 1000,
-                        name : 'assets/img/[name].[ext]'
-                    }
-                }
-            ]
-          }
-        ],
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+    
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],},
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
-      // pass all js files through Babel
-      resolve: {
-        extensions: ["*", ".js", ".jsx"],    // <-- added `.jsx` here
-    },
-    // to work with lucid-cardano
-    experiments: {
-        "asyncWebAssembly": true,
-        "topLevelAwait": true,
-        "layers": true // optional, with some bundlers/frameworks it doesn't work without
-    }
-  
+    
+    
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  experiments: {
+    syncWebAssembly: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin(), 
+],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    hot: true,
+    historyApiFallback: true,
+    publicPath: '/', 
+    host: '0.0.0.0',
+      disableHostCheck: true,
+    
+  },
 };
