00b43c8c626f36ce828e98012391d179fe755601
eavalo14 <avaloserick97@gmail.com>
2023-06-23 23:32:46 -1000

    adding updates to poll users, issue with this algo is that its very unoptimal for people with huge wallets & people with a lot of assets (O(N^2)). Caching extracted metadata can optimize to O(N)

--- scripts/poll-assets.js
+++ scripts/poll-assets.js
@@ -1,45 +1,107 @@
+const fetch = require("node-fetch");
 
-// get assets
-const getAssets = async () => {
-    dbagsPolicyId = "320eb1b7e2f7127be233d43ff051abfb83684d7bcecb381aa10c238a"
-    let headers = {
-        "project_id": 'mainnetDLGWFVceMET9Pok1OwwioXT6iiHpO1iJ',
-    };
-    const address = "addr1qyg3278vaa92lftpf5x3l30lsyjzedvzjpdr3aetnvjvfe0u3jv9a6thqhl78emaank57ysk63yrg0wnrnu65su9uplsdgasyx"
+// global variables to test with 
+// const address = "addr1q87yt2za4kffkdhsfzdu7vdvneqmq88g2lhz05fws8n6dz0u3jv9a6thqhl78emaank57ysk63yrg0wnrnu65su9uplsd6ahr0"
+// good test
+const address = "addr1q80pljq9mqjlx23dn6ryny7enfrf24k5v3aefn4pxds9s7edmsmpfaq4mzhh5jmyhsdk8h68xczw0k9r3n0vcxllz79q3vksqr"
+
+const dbagsPolicyId = "320eb1b7e2f7127be233d43ff051abfb83684d7bcecb381aa10c238a"
+
+// hide api keys
+let headers = {
+    "project_id": 'mainnetDLGWFVceMET9Pok1OwwioXT6iiHpO1iJ',
+};
+
+// extracts stake address from base64 recieving address
+const extractStakeAddress = async (address) => {
     const addressEndpoint = `https://cardano-mainnet.blockfrost.io/api/v0/addresses/${address}`
-
+    
     let response = await fetch(addressEndpoint, {
         headers,
     })
     
     let data = await response.json()
     // get stake address
-    const stakeAddress = data.stake_address
+    return data.stake_address
+}
 
-    // get assets 
-    const assetsEndpoint = `https://cardano-mainnet.blockfrost.io/api/v0/accounts/${stakeAddress}/addresses/assets`
-    response = await fetch(addressEndpoint, {
+
+// read in assets unit value from a page
+const getAssetsPagination = async (page, stakeAddress) => {
+
+    const assetsEndpoint = `https://cardano-mainnet.blockfrost.io/api/v0/accounts/${stakeAddress}/addresses/assets?page=${page}`
+    response = await fetch(assetsEndpoint, {
         headers,
     })
     data = await response.json()
-    const assets = data.amount
-    // console.log(data.amount)
-
-    for (let i = 1; i < assets.length; i++) {
-        const asset = assets[i]
-        const assetEndpoint = `https://cardano-mainnet.blockfrost.io/api/v0/assets/${asset.unit}`
-        response = await fetch(assetEndpoint, {
-            headers,
-        })
-        
-        data = await response.json()
-        console.log(data.policy_id)
-        // if (data.policy_id === dbagsPolicyId) {
-
-            // console.log(data)
-        // }
-    }
-
+    
+    return data
 }
 
-getAssets()
+// get asset information from unit 
+const getAssetInformation = async (unit) => {
+    const assetEndpoint = `https://cardano-mainnet.blockfrost.io/api/v0/assets/${unit}`
+    response = await fetch(assetEndpoint, {
+        headers,
+    })
+        
+    data = await response.json()
+    return data
+}
+
+
+// iterate and get all assets 
+const getAllAssetUnits = async (stakeAddress,projectPolicyID) => {
+    console.log("polling assets")
+    let assets = []
+    // iterate through all assets until size read back is < 100 (max per page)
+    let iteratedAllAssets = false
+    // debug
+    let pageCnt = 0
+    while (!iteratedAllAssets)
+    {
+        console.log(`polling assets in page: ${pageCnt}`)
+        pageCnt += 1;
+        const assetsUnit = await getAssetsPagination(pageCnt,stakeAddress)
+        // iterate through each asset and figure out policy id
+        for (let i = 0; i < assetsUnit.length; i++) {
+            const unit = assetsUnit[i].unit
+            const policyID = unit.substring(0,56)
+            // verify policyid exists and poll for information based on unit
+            if (policyID === projectPolicyID)
+            {
+                // get full asset details and append to array
+                const assetDetails = await getAssetInformation(assetsUnit[i].unit)
+                assets.push(assetDetails)
+            }
+        }
+
+        // break loop if we hit end of pagination
+        iteratedAllAssets = assetsUnit.length < 100 ? true: false;
+        // iteratedAllAssets = true
+    }
+    return assets
+
+}
+// get all policy id assets within an address
+const getAssets = async (address, policyID) => {
+    // find all the assets
+    // extract stakeAddress
+    const stakeAddress = await extractStakeAddress(address)
+    // return all assets with metadata
+    const assets = await getAllAssetUnits(stakeAddress, policyID)
+
+    return assets
+
+}
+// main function 
+const main = async () => {
+    // getting all assets
+    const dbagAssets = await getAssets(address, dbagsPolicyId);
+    // printing first asset as an example
+    console.log(dbagAssets[1])
+    console.log(`total dbags collected: ${dbagAssets.length}`)
+}
+
+
+main()
