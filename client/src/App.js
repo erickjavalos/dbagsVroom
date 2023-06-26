ce8bcf7cf885b010ea20a6365eb26bdbed6a7556
Andres Garcia <51844701+agarcia31@users.noreply.github.com>
2023-06-26 01:37:43 -0700

    Revert "Front"

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from "react";
-import DateTimePicker from "react-datetime-picker";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import DateTimePicker from 'react-datetime-picker';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
+import './App.css';
+import Login from './pages/Login';
+import Authentication from './components/Authentication/index.js';
+import Home from './pages/Home';
 
-import Login from "./pages/Login";
-import Authentication from "./components/Authentication/index.js";
-import Home from "./pages/Home";
-import Minting from "./pages/Minting";
 
 // import input from './input.css' ;
-import "./dist/output.css";
+import './dist/output.css'
+
+
 
 export default function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/Login" element={<Login />} />
-          {/* authentication */}
-          <Route path="/auth" element={<Authentication />} />
-          {/* default homepage */}
-          <Route path="/Home" element={<Home />} />
-          <Route path="/minting" element={<Minting />} />
-        </Routes>
-      </Router>
-    </>
-  );
+
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/Login" element={<Login />} />
+                    {/* authentication */}
+                    <Route path="/auth" element={<Authentication />} />
+                    {/* default homepage */}
+                    <Route path="/Home" element={<Home />} />
+                </Routes>
+            </Router>
+        </>
+    )
+
 }
+
+
+
+    
