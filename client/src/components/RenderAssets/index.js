53cd5ea836b349b6effac3179d2c8d0f529cb73c
eavalo14 <avaloserick97@gmail.com>
2023-07-03 19:28:20 -0700

    fixed carousel and now can render in increments of 3

--- client/src/components/RenderAssets/index.js
+++ client/src/components/RenderAssets/index.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { LazyLoadImage } from "react-lazy-load-image-component";
 
+
+// Render Assets
+//
+// inputs: <data from database>
+// assets: {
+//   _id: <val>
+//   
+//}
+// name: <String> project name
+// 
 
 const RenderAssets = ({ assets, name }) => {
     const [assetMeta, setAssetMeta] = useState([])
     const [currentSlide, setCurrentSlide] = useState(0)
-    const [loading, setLoading] = useState(true);
-
-    // const counter = useRef(0);
-
 
     // set values based on assets we recieve
     useEffect(() => {
@@ -29,12 +34,7 @@ const RenderAssets = ({ assets, name }) => {
         console.log("next slide")
         setCurrentSlide(currentSlide + 1)
     }
-    const imageLoaded = () => {
-        counter.current += 1;
-        if (counter.current >= urls.length) {
-            setLoading(false);
-        }
-    }
+
     return (
         <>
             {/* header name of asset */}
@@ -47,38 +47,98 @@ const RenderAssets = ({ assets, name }) => {
                 <>
                     {/* add left arrow */}
                     <div className="flex flex-row w-11/12 bg-[rgb(123,105,171)] text-sm items-center justify-center p-2 rounded-lg">
-
+                        {/* left arrow (<<) */}
                         <button
-                            className="mr-2 px-2 py-1 text-white bg-[rgba(217,217,217,0.5)] rounded-lg"
+                            className="m-2 px-2 py-1 text-white bg-[rgba(217,217,217,0.5)] rounded-lg"
                             onClick={previousSlide}
                             disabled={currentSlide === 0}
                         >
                             {"<<"}
                         </button>
-                        {/* {console.log(assets)} */}
-                        {/* {console.log(assetMeta.slice(currentSlide*3, currentSlide*3 + 3))} */}
-                        {console.log(!(currentSlide * 3 < assetMeta.length))}
+                        {/* image cards  */}
                         {assetMeta
                             .slice(currentSlide * 3, currentSlide * 3 + 3)
-                            .map((asset) =>
-                            (
-                                <div key={asset._id} className="w-2/4 bg-[rgba(217,217,217,0.5)] m-2 rounded-lg">
-                                    <img className="rounded-lg"
-                                        src={`https://ipfs.io/ipfs/${asset.onchain_metadata.image.split("ipfs://")[1]}`}
-                                        onLoad={imageLoaded}
-                                    >
-                                    </img>
-                                    {asset.onchain_metadata.name}
-                                </div>
-                            ))
+                            .map((asset) => {
+                                // get length of slice 
+                                const assetsWindow = assetMeta.slice(currentSlide * 3, currentSlide * 3 + 3)
+                                // express images normally
+                                if (assetsWindow.length === 3) {
+                                    return (
+                                        <div key={asset._id} className="w-1/2 bg-[rgba(217,217,217,0.5)] m-2 rounded-lg">
+                                            <img className="rounded-lg"
+                                                src={`https://ipfs.io/ipfs/${asset.onchain_metadata.image.split("ipfs://")[1]}`}
+                                                onLoad={imageLoaded}
+                                            >
+                                            </img>
+                                            {asset.onchain_metadata.name}
+                                        </div>
+                                    )
+                                }
+                                // render left for two items in window
+                                else if (assetsWindow.length === 2) {
+                                    if (asset._id !== assetsWindow[1]._id)
+                                    {
+                                        return (
+                                            <div key={asset._id} className="flex flex-row items-center justify-center">
+                                                <div className="w-1/2 bg-[rgba(217,217,217,0.5)] m-2 rounded-lg">
+                                                    <img className="rounded-lg"
+                                                        src={`https://ipfs.io/ipfs/${asset.onchain_metadata.image.split("ipfs://")[1]}`}
+                                                        onLoad={imageLoaded}
+                                                    >
+                                                    </img>
+                                                    {asset.onchain_metadata.name}
+                                                </div>
+                                                <div className="w-1/2 bg-[rgba(217,217,217,0.5)] m-2 rounded-lg">
+                                                    <img className="rounded-lg"
+                                                        src={`https://ipfs.io/ipfs/${assetsWindow[1].onchain_metadata.image.split("ipfs://")[1]}`}
+                                                        onLoad={imageLoaded}
+                                                    >
+                                                    </img>
+                                                    {assetsWindow[1].onchain_metadata.name}
+                                                </div>
+
+                                                <div className="w-1/2 bg-[rgba(217,217,217,0.5)] m-2 rounded-lg">
+                                                </div>
+    
+                                            </div>
+    
+                                        )
+                                    }
+                                }
+                                // render for one asset left in window
+                                else {
+                                    return (
+                                        <div key="1" className="flex flex-row w-11/12 items-center justify-center">
+
+                                            <div key={1} className="w-1/2 bg-[rgba(217,217,217,0.5)] m-2 rounded-lg">
+                                            </div>
+                                            <div key={asset._id} className="w-1/2 bg-[rgba(217,217,217,0.5)] m-2 rounded-lg">
+                                                <img className="rounded-lg"
+                                                    src={`https://ipfs.io/ipfs/${asset.onchain_metadata.image.split("ipfs://")[1]}`}
+                                                    onLoad={imageLoaded}
+                                                >
+                                                </img>
+                                                {asset.onchain_metadata.name}
+                                            </div>
+
+                                            <div key={2} className="w-1/2 bg-[rgba(217,217,217,0.5)] m-2 rounded-lg">
+                                            </div>
+                                        </div>
+                                    )
+                                }
+
+                            })
                         }
+                        {/* right arrow (>>) */}
                         <button
-                            className="mr-2 px-2 py-1 text-white bg-[rgba(217,217,217,0.5)] rounded-lg"
+                            className="m-2 px-2 py-1 text-white bg-[rgba(217,217,217,0.5)] rounded-lg"
                             onClick={nextSlide}
                             disabled={!((currentSlide + 1) * 3 < assetMeta.length)}
                         >
                             {">>"}
                         </button>
+
+
                     </div>
                     {/* add right arrow */}
                 </>
