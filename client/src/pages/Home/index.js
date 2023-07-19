ad64d2187045800760a4a54a95f067f3143ed9ee
eavalo14 <avaloserick97@gmail.com>
2023-07-19 09:40:46 -0700

    adding updates to code

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,12 +3,7 @@ import Header from "../../components/Header";
 import Auth from "../../utils/auth";
 import BuildMfer from "../../components/BuildMfer";
 import backgroundImage from "../../images/background_auto.png"
-// import backgroundImage from "../../assets/auto_assets/Background/Trippymferforest.png"
 
-
-import NamiWalletApi, { Cardano } from "../../nami-js";
-import blockfrostApiKey from "../../../config.js";
-let nami;
 
 
 const styles = {
@@ -141,107 +136,7 @@ function Home() {
     return testdata;
   };
 
-  const processMintRequest = async () => {
-    // get hashed metadata
-    console.log("gettin address");
-    // instantiate serialization lib that helps decode blockchain data
-    const S = await Cardano();
-    // initialize nami wallet helper class
-    nami = new NamiWalletApi(
-      S,
-      window.cardano,
-      blockfrostApiKey,
-      walletConnected
-    );
-
-    // GET hashed metadata
-    const mint = await fetch("/api/mint/", {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    // extract data
-    const data = await mint.json();
-    // extract hashed metadata
-    const hashedMeta = data.metaDataHash;
-
-    // extract payment address
-    let paymentAddress = await nami.getAddress(); // nami wallet address
-
-    // ********************************************
-    // Build Transaction
-    // ********************************************
-
-    // build recipients
-    let recipients = [
-      {
-        address:
-          "addr_test1qrnns8ctrctt5ga9g990nc4d7pt0k25gaj0mnlda320ejmprlzyh4mr2psnrgh6ht6kaw860j5rhv44x4mt4csl987zslcr4p6",
-        amount: "10",
-      }, // Seller Wallet, NFT price 10ADA
-      {
-        address: paymentAddress,
-        amount: "0",
-        mintedAssets: [
-          {
-            assetName: "Test1",
-            quantity: "1",
-            policyId:
-              "91d319c0fc8c557244d2ac5c2d1c0cbeaeb40a13804f122a51705da1",
-            policyScript:
-              "8201828200581c98d6a076c31a9d248ec8fe5459682f2ec2623cf376ad0c1c5a61237b82051a02d518fb",
-          },
-        ],
-      }, // NFTs to be minted
-    ]; // list of recipients
-    // build fake metadata to help estimate gas fees (use maximum packet size to be safe)
-    let dummyMetadata = {
-      721: {
-        // policyId
-        "36aa169af7dc9bb5a566987191221f2d7a92aab211350f7119fc1541": {
-          // NFTName
-          Test1: {
-            name: "sfgsdfgdfsg",
-            description: "gsdffsgdfsgdfsgdfsg",
-            image:
-              "isdgdfsgafsgdfdfsgdfsgdfsgdfsgdfsgdfsgdfsgdfgdfgdfsgdfsgdfsgdfsg",
-          },
-        },
-      },
-    };
-
-    console.log("building transaction...");
-    // combine and build transaction
-    let transaction = await nami.transaction({
-      PaymentAddress: paymentAddress,
-      recipients: recipients,
-      metadata: dummyMetadata,
-      metadataHash: hashedMeta,
-      addMetadata: false,
-      utxosRaw: await nami.getUtxosHex(),
-      multiSig: true,
-    });
-    console.log("SUCCESS: transaction built!");
-
-    // prompt user to sign and retreive signature hash
-    console.log("Prompting user to sign");
-    const witnessBuyer = await nami.signTx(transaction, true);
-    console.log("SUCCESS: user signed transaction");
-
-    // send witness buyer signature and transaction to backend to submit to chain
-    console.log("Asset minting...");
-    const processMint = await fetch("/api/mint/processMint", {
-      method: "POST",
-      body: JSON.stringify({
-        witnessBuyer: witnessBuyer,
-        transaction: transaction,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  };
+ 
   // helper functions
   const logout = (event) => {
     event.preventDefault();
