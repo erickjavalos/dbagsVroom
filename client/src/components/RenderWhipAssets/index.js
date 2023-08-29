25e098f44bda6fd299cb9530a21d4e6b94004381
eavalo14 <avaloserick97@gmail.com>
2023-08-29 12:07:11 -0700

    cleaning up

--- client/src/components/RenderWhipAssets/index.js
+++ client/src/components/RenderWhipAssets/index.js
@@ -11,6 +11,7 @@ const RenderWhipAssets = ({ assets, assetSelected, setAssetSelected, minted, set
         <>
             {/* assets in wallet per window */}
             {assets.length > 0 ? (
+
                 // render assets
                 <>
                     <RenderWhipSubAssets
@@ -23,9 +24,14 @@ const RenderWhipAssets = ({ assets, assetSelected, setAssetSelected, minted, set
                 </>
             ) : (
                 <>
-                    <div className="flex flex-row w-4/5 bg-[rgb(123,105,171)] text-sm items-center justify-center p-2 rounded-lg">
-                        <div className="w-2/4 bg-[rgba(217,217,217,0.5)] m-2 rounded-lg">
-                            <h1 className="text-red">You do not hold any dbags</h1>
+                    <div className="flex flex-col items-center">
+                        <div className="justify-center items-center w-2/5 bg-[rgb(96,107,171)] text-2xl rounded-lg m-2">
+                            whips
+                        </div>
+                        <div className="flex flex-row w-4/5 bg-[rgb(123,105,171)] text-sm items-center justify-center p-2 rounded-lg">
+                            <div className="w-2/4 bg-[rgba(217,217,217,0.5)] m-2 rounded-lg">
+                                <h1 className="text-red">You do not hold any whips</h1>
+                            </div>
                         </div>
                     </div>
 
