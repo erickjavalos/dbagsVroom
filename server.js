14664274a6f6ca307dbdb2948cdf1072cb4956f4
eavalo14 <avaloserick97@gmail.com>
2023-08-31 15:18:28 -0700

    working version now!

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ const startApolloServer = async (typeDefs, resolvers) => {
   };
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   })
   
 // Call the async function to start the server
-// startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers);
