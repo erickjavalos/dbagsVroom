c1dd5e4e0f615c0bc5495867c03f968d4d8ab173
eavalo14 <avaloserick97@gmail.com>
2023-08-31 14:20:32 -0700

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const buildPath = path.join(__dirname, './dist');
 // const { typeDefs, resolvers } = require('./schema');
 // const {db} = require('./config/connection');
 
-// const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 
 const app = express();
 
